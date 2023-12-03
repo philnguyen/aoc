62564087c4622503590a76cbed94e35a60ae5980
Phil Nguyen <philnguyen0112@gmail.com>
2023-12-03 00:44:31 -0800

    More preps

--- 2023/Lib.lean
+++ 2023/Lib.lean
@@ -3,7 +3,6 @@ import Lean.Data.PersistentHashSet
 
 open Lean
 
-abbrev ℘ (α : Type) [BEq α] [Hashable α] := PersistentHashSet α
 abbrev ℕ := Nat
 abbrev ℤ := Int
 
@@ -19,6 +18,14 @@ abbrev Index : ℕ → Type
 abbrev Vec : ℕ → Type → Type
 | 0, t => t
 | n + 1, t => List (Vec n t)
+
+private def Index.beq : Index n → Index n → Bool :=
+  match n with
+  | 0 => BEq.beq
+  | 1 => BEq.beq
+  | _ + 2 => λ ⟨i₀, i⟩ ⟨j₀, j⟩ => i₀ == j₀ && beq i j
+
+instance : BEq (Index n) where beq := Index.beq
 
 def Vec.ref (default : t)  (vec : Vec n t) (i : Index n) : t :=
   match n with
@@ -54,35 +61,54 @@ section Test
 end Test
 
 -- `Index 2` interpreted as `row × col` as opposed to `x × y`
-abbrev Move n := Index n → Index n
-def Move.all_basis_moves : List (Move n) := match n with
+def Index.straight_adjacents (i : Index n) : List (Index n) :=
+  match n with
   | 0 => []
-  | 1 => [(· - 1), (· + 1)]
-  | m + 2 => (λ ⟨i₀, i⟩ => (i₀ - 1, i)) ::
-             (λ ⟨i₀, i⟩ => (i₀ + 1, i)) ::
-             (all_basis_moves (n := m + 1)).map (λ ⟨i₀, i⟩ => ⟨i₀, · i⟩)
+  | 1 => [(i - 1), (i + 1)]
+  | _ + 2 => let (i₀, i') := i
+             (i₀ - 1, i') ::
+             (i₀ + 1, i') ::
+             i'.straight_adjacents.map (⟨i₀, ·⟩)
 
-def Move.all_moves : List (Move n) := match n with
-  | 0 => []
-  | 1 => [(· - 1), (· + 1)]
-  | m + 2 => let sub_moves := all_moves (n := m + 1)
-             sub_moves.map (λ ⟨i₀, i⟩ => ⟨i₀ - 1, · i⟩) ++
-             sub_moves.map (λ ⟨i₀, i⟩ => ⟨i₀ + 1, · i⟩)
+def Index.adjacents (i : Index n) : List (Index n) :=
+  let rec go (i : Index n) : List (Index n) :=
+    match n with
+    | 0 => []
+    | 1 => [i - 1, i + 1]
+    | m + 2 => let ⟨i₀, i'⟩ := i
+               let adjacents' := i'.adjacents
+               (i₀ - 1, i') ::
+               (i₀ + 1, i') ::
+               adjacents'.map (i₀ - 1, ·) ++
+               adjacents'.map (i₀, ·) ++
+               adjacents'.map (i₀ + 1, ·)
+  (go i).filter (· != i)
 
 section Test
-  def ex_basis_moves3 := (Move.all_basis_moves (n := 3)).map (· (0, 0, 0))
-  example : ex_basis_moves3.length = 6 := rfl
-  example : [(0, 0, -1), (0, 0, 1), (0, -1, 0), (0, 1, 0), (-1, 0, 0), (1, 0, 0)].all ex_basis_moves3.contains
+  def origin3 : Index 3 := (0, 0, 0)
+  example : origin3.straight_adjacents.length = 6 := rfl
+  example : [(0, 0, -1), (0, 0, 1), (0, -1, 0), (0, 1, 0), (-1, 0, 0), (1, 0, 0)].all origin3.straight_adjacents.contains
     := rfl
 
-  def ex_moves3 := (Move.all_moves (n := 3)).map (· (0, 0, 0))
-  example : ex_moves3.length = 8 := rfl
+  def origin2 : Index 2 := (0, 0)
+  example : origin2.adjacents.length = 8 := rfl
   example : [-1, 1].all $ λ i₀ =>
             [-1, 1].all $ λ i₁ =>
-            [-1, 1].all $ λ i₂ =>
-            ex_moves3.contains (i₀, i₁, i₂)
+            origin2.adjacents.contains (i₀, i₁)
     := rfl
 end Test
+
+def Index.add (v₁ v₂ : Index n) : Index n :=
+  match n with
+  | 0 => ()
+  | 1 => v₁ + v₂
+  | _ + 2 => let ⟨n₁, v₁'⟩ := v₁
+             let ⟨n₂, v₂'⟩ := v₂
+             ⟨n₁ + n₂, add v₁' v₂'⟩
+
+
+instance : Add (Index n) where
+  add := Index.add
 
 abbrev SparseGrid := PersistentHashMap (Index 2)
 
@@ -94,6 +120,8 @@ partial
 def IO.FS.Stream.lines (str : IO.FS.Stream): IO (List String) := do
   let l ← str.getLine
   if l.length == 0 then return [] else do let ls ← str.lines; return l.trim :: ls
+
+def stdin_lines : IO (List String) := IO.getStdin >>= IO.FS.Stream.lines
 
 def List.map_splitted_lines (f : List String → α) (lines : List String) (sep : String := " ") : List α :=
   lines.map (λ l => f (l.splitOn sep))
@@ -154,6 +182,18 @@ section Test
     := rfl
 end Test
 
+def Char.as_digit? (c : Char) : Option ℕ :=
+  if '0' ≤ c && c ≤ '9' then c.toNat - '0'.toNat else .none
+
+def Char.is_digit : Char → Bool := Option.isSome ∘ as_digit?
+
+section Test
+  example : '0'.as_digit? = .some 0 := rfl
+  example : '5'.as_digit? = .some 5 := rfl
+  example : '9'.as_digit? = .some 9 := rfl
+  example : 'a'.as_digit? = .none := rfl
+end Test
+
 -----------------------------------------------------------------------
 -- List
 -----------------------------------------------------------------------
@@ -167,6 +207,54 @@ section Test
   example : [2, 3, 4].sum = 9 := rfl
   example : [2, 3, 4].prod = 24 := rfl
 end Test
+
+-----------------------------------------------------------------------
+-- Set
+-----------------------------------------------------------------------
+
+abbrev ℘ (α : Type) [BEq α] [Hashable α] := PersistentHashSet α
+notation "⊘" => PersistentHashSet.empty
+
+def ℘.filter [BEq α] [Hashable α] (p : α → Bool) (s : ℘ α) : ℘ α :=
+  s.fold (λ s x => if p x then s else s.erase x) s
+
+def ℘.map [BEq α] [Hashable α] [BEq β] [Hashable β] (f : α → β) (s : ℘ α) : ℘ β :=
+  s.fold (λ s x => s.insert (f x)) ⊘
+
+def ℘.filterMap [BEq α] [Hashable α] [BEq β] [Hashable β] (f : α → Option β) (s : ℘ α) : ℘ β :=
+  s.fold (λ s x => match f x with
+                   | .some y => s.insert y
+                   | .none => s)
+         ⊘
+
+def ℘.union [BEq α] [Hashable α] (s₁ s₂ : ℘ α) : ℘ α :=
+  if s₁.size ≤ s₂.size then s₁.fold .insert s₂ else s₂.fold .insert s₁
+
+def ℘.intersect [BEq α] [Hashable α] (s₁ s₂ : ℘ α) : ℘ α :=
+  if s₁.size ≤ s₂.size then s₁.filter s₂.contains else s₂.filter s₁.contains
+
+infixl:65 " ∪ " => ℘.union
+infixl:70 " ∩ " => ℘.intersect
+
+def List.toSet [BEq α] [Hashable α] : List α → ℘ α := foldl .insert ⊘
+
+-----------------------------------------------------------------------
+-- Map
+-----------------------------------------------------------------------
+
+infixr:30 " ⊨> " => PersistentHashMap
+
+instance : Hashable Char where
+  hash c := c.toNat.toUInt64
+
+def Lean.PersistentHashMap.update [BEq α] [Hashable α] (k : α) (f : α → β → β) (v : β) (m : α ⊨> β) : α ⊨> β :=
+  m.insert k (f k (match m.find? k with
+                   | .some v => v
+                   | .none => v))
+
+
+def Lean.PersistentHashMap.filter [BEq α] [Hashable α] (p : α → β → Bool) (m : α ⊨> β) : α ⊨> β :=
+  m.foldl (λ m k v => if p k v then m else m.erase k) m
 
 -----------------------------------------------------------------------
 -- State search
@@ -238,3 +326,54 @@ section Test
   example : ∃ q, .some (1, q) = Queue.next (.insert 3 (.insert 2 .(.insert 1 .empty)))
     := by simp; exists ⟨[], [2, 3]⟩
 end Test
+
+-----------------------------------------------------------------------
+-- Union find
+-----------------------------------------------------------------------
+
+structure UnionFind (α : Type) [BEq α] [Hashable α] where
+  parents : PersistentHashMap α α
+  ranks : PersistentHashMap α ℕ
+deriving Inhabited
+
+abbrev UnionFindT (α : Type) [BEq α] [Hashable α] := StateT (UnionFind α)
+
+class MonadUnionFind (α : outParam Type) (m : Type → Type) where
+  union : α → α → m Unit
+  find : α → m α
+
+private def UnionFind.parent_of [BEq α] [Hashable α] : UnionFind α → α → α
+| ⟨parents, _⟩, x => parents.findD x x
+
+private def UnionFind.rank_of [BEq α] [Hashable α] : UnionFind α → α → ℕ
+| ⟨_, ranks⟩, x => ranks.findD x 0
+
+def UnionFind.empty [BEq α] [Hashable α] : UnionFind α := ⟨.empty, .empty⟩
+
+partial
+def UnionFind.find [Monad m] [BEq α] [Hashable α] (x : α) : (UnionFindT α) m α := do
+  let x' := (← get).parent_of x
+  if x == x' then return x
+  let x₁ ← find x'
+  modify (λ uf => {uf with parents := uf.parents.insert x x₁})
+  return x₁
+
+def UnionFind.union [Monad m] [BEq α] [Hashable α] (x y : α) : (UnionFindT α) m Unit := do
+  let x' ← find x
+  let y' ← find y
+  if x' != y' then
+    let uf@⟨parents, ranks⟩ ← get
+    let (x_root, x_root_rank, y_root, y_root_rank) :=
+      let x'_rank := uf.rank_of x'
+      let y'_rank := uf.rank_of y'
+      if x'_rank < y'_rank
+        then (y', y'_rank, x', x'_rank)
+        else (x', x'_rank, y', y'_rank)
+    set $ UnionFind.mk (parents.insert y_root x_root)
+                       (if x_root_rank == y_root_rank
+                         then ranks.insert x_root (1 + x_root_rank)
+                         else ranks)
+
+instance [Monad m] [BEq α] [Hashable α] : MonadUnionFind α (UnionFindT α m) where
+  union := UnionFind.union
+  find := UnionFind.find
