ae1a96a9ae7fe5202a4c70ed0f278383bd010ed8
Phil Nguyen <philnguyen0112@gmail.com>
2023-12-09 12:56:18 -0800

    Clean up P09

--- 2023/P09.lean
+++ 2023/P09.lean
@@ -9,21 +9,17 @@ partial
 def List.derivs (l : List ℤ) : List (List ℤ) :=
   if l.all (· == 0) then [l] else l :: l.deriv.derivs
 
-partial
-def List.last_num (ls : List (List ℤ)) :=
-  let rec go : List (List ℤ) → ℤ
-    | [n :: _] => n
-    | (n₁ :: _) :: (n₂ :: ns₂) :: ls => go (((n₁ + n₂) :: n₂ :: ns₂) :: ls)
-    | _ => panic! "nope!"
-  go (ls.map List.reverse).reverse
+def List.last_num (ls : List (List ℤ)) : ℤ :=
+  ls |>.map last!
+     |>.reverse
+     |>.reduce (· + ·)
+     |>.get!
 
-partial
-def List.first_num (ls : List (List ℤ)) :=
-  let rec go : List (List ℤ) → ℤ
-    | [n :: _] => n
-    | (n₁ :: _) :: (n₂ :: ns₂) :: ls => go (((n₂ - n₁) :: n₂ :: ns₂) :: ls)
-    | _ => panic! "nope!"
-  go ls.reverse
+def List.first_num (ls : List (List ℤ)) : ℤ :=
+  ls |>.map head!
+     |>.reverse
+     |>.reduce (λ n₁ n₂ => n₂ - n₁)
+     |>.get!
 
 def with_extrap (extrap : List (List ℤ) → ℤ) : IO Unit := do
   let parse_line : Parsec (List ℤ) := Parsec.sep_by Parsec.int Parsec.ws
@@ -37,4 +33,4 @@ def with_extrap (extrap : List (List ℤ) → ℤ) : IO Unit := do
 def q1 := with_extrap List.last_num
 def q2 := with_extrap List.first_num
 
-def main : IO Unit := q1
+def main : IO Unit := q2
