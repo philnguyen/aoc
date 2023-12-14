ceeef8a7c7eb6ae528401d8049bc28f252f152d4
Phil Nguyen <philnguyen0112@gmail.com>
2023-12-13 23:33:46 -0800

    P14: run each direction till completion instead

--- 2023/P14.lean
+++ 2023/P14.lean
@@ -11,27 +11,33 @@ def read_map : IO Map := do
   let sharps := m.keys.filter (m.find? · == .some true ) |>.toSet
   return (rounds, sharps, lines.length, lines.head!.length)
 
-def shift (dim : Fin 2) (target : ℤ) (m : Map) : Map :=
+def move (dim : Fin 2) (target : ℤ) (m : Map) : Map :=
   let (rounds, sharps, rows, cols) := m
+  let nudge (pos : Index 2) : Index 2 :=
+    let x := pos.get dim
+    pos.set dim (if x > target then x - 1 else x + 1)
   let rounds' := rounds |>.toList
                         |>.sorted_by (λ pos₁ pos₂ => (target - pos₁.get dim).natAbs < (target - pos₂.get dim).natAbs)
-                        |>.foldl (λ rounds' pos =>
-                                    let x := pos.get dim
-                                    let pos' := pos.set dim (if x > target then x - 1 else x + 1)
-                                    rounds'.insert (if x = target ∨ rounds.contains pos' ∨ sharps.contains pos' then pos else pos'))
+                        |>.foldl (λ rounds' pos => Id.run $ do
+                                    let mut pos := pos
+                                    let mut pos' := nudge pos
+                                    while pos.get dim != target ∧ ¬ rounds'.contains pos' ∧ ¬ sharps.contains pos' do
+                                      pos := pos'
+                                      pos' := nudge pos'
+                                    rounds'.insert pos)
                                  ∅
   (rounds', sharps, rows, cols)
 
-def shift_north : Map → Map                  := shift 0 0
-def shift_west  : Map → Map                  := shift 1 0
-def shift_south : Map → Map | m@⟨_, _, r, _⟩ => shift 0 (r - 1) m
-def shift_east  : Map → Map | m@⟨_, _, _, c⟩ => shift 1 (c - 1) m
-def cycle := fix shift_east ∘ fix shift_south ∘ fix shift_west ∘ fix shift_north
+def move_north : Map → Map                  := move 0 0
+def move_west  : Map → Map                  := move 1 0
+def move_south : Map → Map | m@⟨_, _, r, _⟩ => move 0 (r - 1) m
+def move_east  : Map → Map | m@⟨_, _, _, c⟩ => move 1 (c - 1) m
+def cycle := move_east ∘ move_south ∘ move_west ∘ move_north
 
 def Map.weight : Map → ℤ
 | (rounds, _, n, _) => rounds.sum_by (λ ⟨r, _⟩ => n - r)
 
 def main := do
   let m ← read_map
-  IO.println s!"q1: {(fix shift_north m).weight}"
+  IO.println s!"q1: {(move_north m).weight}"
   IO.println s!"q2: {(cached_iter 1000000000 cycle m).weight}"
